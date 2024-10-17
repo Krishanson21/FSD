9d7e16f1f70c0c2c8171d0f745f737aef69c797c
Krishanson21 <krishantson21@gmail.com>
2024-10-16 23:14:24 -0700

    first commit

---
+++ jswork/index.js
@@ -0,0 +1,79 @@
+// console.log("welcome to JS");
+// var a=12;
+// console.log(a);
+// if(a>10)
+// {
+//     let b=23; //work in scope
+//     a=40
+//     console.log("a inside condition:"+a);
+// }
+// console.log("b outside condition"+b);
+// let a=12;
+// let b="12";
+// console.log(typeof b);
+// let today=Date();
+// console.log(today);
+// if(a==b)    //works on value
+// {
+//     console.log("Welcome");
+// }
+// else{
+//     console.log("Hello");
+// }
+// if(a===b)
+// {
+//     console.log("welcome");
+// }
+// else{
+//     console.log("Hello");
+// }
+// let myname="Amit";
+// let college="ABES";
+// let result=Hi,my name is ${myname} and I m in ${college};
+// console.log(result);
+
+//objects is JS
+// let key='sname'
+// const students={
+//     sname:"Amit",
+//     course:"Btech",
+//     college:"ABES EC"
+// }
+// console.log(students);
+// console.log("Name:"+students.sname+"  course:"+students.course);
+// const {sname}=students;
+// console.log(sname);
+// students[key]='Rahul'
+// console.log(students);
+
+// function greeting(msg)
+// {
+//     console.log("good morning"+msg)
+// }
+// greeting(" welcome");
+// function sum(a,b)
+// {
+//     return a+b;
+// }
+// console.log(sum(12,28));
+
+// function cCompiler()
+// {
+//     return "cCompiler selected";
+// }
+// function javaCompiler()
+// {
+//     return "javaCompiler selected";
+// }
+// function selectCompiler(clbk)
+// {
+//     console.log("you have :"+clbk());
+// }
+// selectCompiler(javaCompiler);
+// selectCompiler(c+Compiler);
+
+console.log("hello world");
+console.log(document);
+let parent=document.getElementsByClassName("parent")
+console.log(parent);
+paremt[0].innerText=" ch2";
