c28754ad4fc7ef88959d6353e6efdef16b352d88
Krishanson21 <krishantson21@gmail.com>
2024-12-09 01:55:50 -0800

    yo

--- react/y/src/App.jsx
+++ react/y/src/App.jsx
@@ -1,86 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react'
 import Student from './Student';
-import './student.css';
-import Usestudentstate from './Usestudentstate';
+import UseStudentState from './UseStudentState';
 import Imagemanipulation from './Imagemanipulation';
+import MyUseEffect from './MyUseEffect';
 import UseFetchAPI from './UseFetchAPI';
 import Login from './Login';
+import Registration from './Registration';
+function App() {
 
-function HelloWorld() {
-  const h1 = <h1>Hello World!</h1>
-  const mystyle  ={
-    color:'white',
-    backgroundColor : 'purple'
-  }
+  const[rdata,setrdata]=useState();
+  const h1=<h1>Hello World</h1>;
+ const mystyle={
+  color:'red',
+  backgroundColor:'yellow'
+ }
 
-  const StudentData = [{
-    college : "ABES Engineering College",
-    Name : "Tom",
-    pic : <img src='https://media.istockphoto.com/id/1369754239/photo/university-student-in-white-background-stock-photo.jpg?s=612x612&w=0&k=20&c=LjFVDfjusWBjYTNliHV9DyXfApPGc8DmgBGEtfVgQ0Q=' height={100} width={100} />,
-    branch : "CSE",
-    roll : 1,
-    section : "B"}
-  ,
-  {
-    college : "ABES Engineering College",
-    Name : "Tammy",
-    pic : <img src='https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/srixner.png' height={100} width={100} />,
-    branch : "CSE",
-    roll : 3,
-    section : "B"
-  },
-  {
-    college : "ABES Engineering College",
-    Name : "Tan",
-    pic : <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZ47HILWuUgVTqOxL-lJ0Jdvo9tpUNIP5X5Q&s' height={100} width={100} />,
-    branch : "CSE",
-    roll : 2,
-    section : "B"
-  }
-]
+
+ 
   return (
     <div>
-      <h2>Hello</h2>
-    <div>
-      {/* <Usestudentstate></Usestudentstate> */}
-      <Login></Login>
+      <div>
+        {JSON.stringify(rdata)}
+      </div>
+   <h2>Hello Using useSatte Hook</h2>
+  <div>
+    {/* <Login /> */}
+    <Registration regData={setrdata} />
+    {/* <UseStudentState /> */}
+    {/* <Imagemanipulation /> */}
+    {/* <MyUseEffect /> */}
+    {/* <UseFetchAPI /> */}
+    {/* <Login /> */}
+  <div>
+    <h2>Login Form</h2>
+    <Login regdata={rdata} />
+  </div>
+   
+  </div>
     </div>
-    </div>
-    // <div style={{backgroundColor : 'orange'}}>
-    //   {h1}
-    //   <div style={mystyle}>
-    //     ABES Engineering College
-    //   </div>
-    //   <div style={{display : 'flex'}}>
-    //      {
-    //       StudentData.map(
-    //         (ele) => {
-    //           return <Student data = {ele} />
-    //         }
-    //       )         
-    //      }
-    //     {/*<Student 
-    //     name = 'Harshil' 
-    //     pic = {<img src='https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/srixner.png' height={100} width={100} />}
-    //     branch = ' CSE' 
-    //     roll = '2200320100069' 
-    //     section='B'/>
-
-    //   <Student college = 'ABES Enginerring College' 
-    //     name = 'Prabhat' 
-    //     pic = {<img src='https://media.istockphoto.com/id/1369754239/photo/university-student-in-white-background-stock-photo.jpg?s=612x612&w=0&k=20&c=LjFVDfjusWBjYTNliHV9DyXfApPGc8DmgBGEtfVgQ0Q=' height={100} width={100} />}
-    //     branch = ' CSE' 
-    //     roll = '2200320100112' 
-    //     section='B'/>
-
-    //   <Student college = 'ABES Enginerring College' 
-    //     name = 'Priyanshu' 
-    //     pic = {<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZ47HILWuUgVTqOxL-lJ0Jdvo9tpUNIP5X5Q&s' height={100} width={100} />}
-    //     branch = ' CSE' 
-    //     roll = '2200320100169' 
-    //     section='B'/>*/}
-    //     </div>
-    //   </div>
-  );
+      
+  )
 }
-export default HelloWorld;
+
+export default App
